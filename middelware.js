face1393a3a15252ff09b4ed33da10ca656ac60e
C Busch <chrisb.personal@gmail.com>
2017-07-08 14:50:59 -0400

    Port update- heroku

---
+++ middelware.js
@@ -0,0 +1,16 @@
+// use mdoeule exports to expose middelware
+// 
+// 
+var middleware = {
+	requireAuthentication: function(req, res, next) {
+		console.log('private route hit');
+		next();
+	}, 
+	logger: function (req, res, next) {
+
+		console.log('Request : ' + new Date().toString() + ' ' + req.method + ' ' + req.originalUrl);
+		next();
+	}
+};
+
+module.exports = middleware;
